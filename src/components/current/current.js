6997eb7aeb40727013cd5eb7ff946d15bb762f34
Ken Bellows <ken.bellows@live.com>
2015-09-27 20:45:44 -0400

    switched to bower for frontend dependencies

--- src/components/current/current.js
+++ src/components/current/current.js
@@ -4,11 +4,29 @@ angular.module('nfdash.current', [])
     startDate: '2015-09-14',
     endDate: '2015-10-26',
   };
-  this.dateRange = function() {
-    return (
-      $moment(this.challenge.startDate).format('dddd MMMM D') +
-      ' - ' +
-      $moment(this.challenge.endDate).format('dddd MMMM D')
-    );
+  this.dateRange = (
+    $moment(this.challenge.startDate).format('dddd MMMM D') +
+    ' - ' +
+    $moment(this.challenge.endDate).format('dddd MMMM D')
+  );
+  
+  this.data = {
+    // pushups 3x/wk
+    quest1: {
+      labels: [
+        'Week 1',
+        'Week 2',
+        'Week 3',
+        'Week 4'
+      ],
+      
+    },
+    quest2: {
+    },
+    quest3: {
+    },
+    quest4: {
+
+    }
   };
 }]);
